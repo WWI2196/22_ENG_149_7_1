02cb0083f40b5e0c99b18bb1612121b76467ac0f
WansajeeWI <127069062+WW221529@users.noreply.github.com>
2024-02-04 23:26:15 +0530

    mode coded successfully

--- 22_ENG_149_7_1.cpp
+++ 22_ENG_149_7_1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 float mean(int* table, int size);
 float median(int* table, int size);
-int mode(int* table, int size);
+void mode(int* table, int size);
 
 float mean(int* table, int size) {
 	float sum = 0;
@@ -33,9 +33,9 @@ float median(int* table, int size) {
 	}
 }
 
-int mode(int* table, int size) {
-    
-    int* temp= new int[size];
+void mode(int* table, int size) {
+
+    int* temp = new int[size];
 
     for (int i = 0; i < size; i++) {
         int count = 0;
@@ -44,22 +44,64 @@ int mode(int* table, int size) {
                 continue;
             }
             else if (*(table + i) == *(table + j)) {
-				count++;
-			}
-		}
+                count++;
+            }
+        }
         *(temp + i) = count;
-	}
+    }
 
-	int max = 0;
-	for (int i = 0; i < size; i++) {
-        if (*(temp + i) > max) {
-			max = *(temp + i);
-		}
-	}
+    bool modeExist = false;
+    for (int i = 0; i < size; i++) {
+        if (*(temp + i) > 0) {
+            modeExist = true;
+            break;
+        }
+    }
 
-	if (max == 0) {
-	}
+    if (modeExist) {
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (max < *(temp + i)) {
+                max = *(temp + i);
+            }
+        }
+
+        int* indexT = new int[size]; // Allocate memory for size elements instead of index
+
+        for (int i = 0; i < size; i++) {
+            if (max == *(temp + i)) {
+                index++;
+                *(indexT + (index - 1)) = i;
+            }
+        }
+
+        if (index == 1) {
+            cout << *(table + *(indexT));
+        }
+        else {
+            cout << "Mode\t:";
+            for (int i = 0; i < index; i++) {
+                bool alreadyPrinted = false;
+                for (int j = 0; j < i; j++) {
+                    if (*(table + *(indexT + i)) == *(table + *(indexT + j))) {
+                        alreadyPrinted = true;
+                        break;
+                    }
+                }
+                if (!alreadyPrinted) {
+                    cout << *(table + *(indexT + i)) << " ";
+                }
+            }
+            cout << endl;
+        }
+        delete[] indexT;
+    }
+
+    delete[] temp;
 }
+
 
 int main() {
     while (true) {
@@ -79,7 +121,7 @@ int main() {
 
         cout << "Mean\t:" << mean(table, numberOfSt) << endl;
         cout << "Median\t:" << median(table, numberOfSt) << endl;
-        cout << "Mode\t:" << mode(table, numberOfSt) << endl;
+        mode(table, numberOfSt);
         
         
         delete[] table;
